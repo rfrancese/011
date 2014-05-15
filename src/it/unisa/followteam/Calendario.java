761aacd71d6a28c4624e446d59d05873a9cb4314
Rita D'Ambrosio <r.dambrosio23@studenti.unisa.it>
2014-05-15 17:16:18 +0200

    aggiunto nome user e logo in calendario
    Signed-off-by: Rita D'Ambrosio <r.dambrosio23@studenti.unisa.it>

--- src/it/unisa/followteam/Calendario.java
+++ src/it/unisa/followteam/Calendario.java
@@ -13,8 +13,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Calendario  extends Fragment {
 
@@ -47,69 +49,76 @@ public class Calendario  extends Fragment {
 
 	    db.close();
 
-	    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-	    	 @Override
-	    	    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-	    		 	Cursor cursor = (Cursor) arg0.getItemAtPosition(arg2);
-	    		 	giornataScelta = cursor.getString(0);
-
-
-	    		 	db.open();
-	    		 	String sql="SELECT "+ MyDatabase.PartiteMetaData.PARTITA_ID
-	    		 			+", "+MyDatabase.PartiteMetaData.PARTITA_SQ_CASA
-	    		 			+ ", "+ MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE 
-	    		 			+ " FROM "+ MyDatabase.PartiteMetaData.PARTITA_TITLE_TABLE
-	    		 			+ " WHERE "+MyDatabase.PartiteMetaData.PARTITA_IDGIORNATA
-	    		 			+ "='"+giornataScelta+"'";
-	    		 	Cursor c = db.query(sql, null);
-
-	    		 	SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-	    			    		rootView.getContext(), 
-	    			    		R.layout.list_item_view_partite, 
-	    			    		c, 
-	    			    		new String[]{MyDatabase.PartiteMetaData.PARTITA_SQ_CASA, MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE}, 
-	    			    		new int[] {R.id.sqCasa, R.id.sqOspite},
-	    			    		1);
-
-
-	    		 	listaPartite.setAdapter(adapter);
-	    			db.close();
-
-	    	 	}
-
-	    	    @Override
-	    	    public void onNothingSelected(AdapterView<?> arg0) {
-	    	        // TODO Auto-generated method stub
-	    	    }   
-	    });
-
-	    listaPartite.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				Cursor c=(Cursor)parent.getItemAtPosition(position);
-				String sqCasa=c.getString(1);
-				String sqOspite=c.getString(2);
-
-				Bundle args = new Bundle();
-				args.putString(MyDatabase.PartiteMetaData.PARTITA_SQ_CASA, sqCasa);
-				args.putString(MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE, sqOspite);
-				args.putString(MyDatabase.PartiteMetaData.PARTITA_IDGIORNATA,giornataScelta);
-				Fragment fragment =new DettagliPartita();
-	 			FragmentManager fragmentManager = getFragmentManager();
-	 			fragment.setArguments(args);
-	 	        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-
-
-			}
-
-		});
-
-
+	    spinner.setOnItemSelectedListener(new MyListenerSpinner());
+	    listaPartite.setOnItemClickListener(new MyListnerListView());
+	    
+	    TextView userlabel = (TextView) rootView.findViewById(R.id.username);
+	    ImageView imageTeam = (ImageView) rootView.findViewById(R.id.imageSquadra);
+	    
+	    userlabel.setText(HomeActivity.ACCOUNT.getUsername());
+	    String nomeSquadra = HomeActivity.ACCOUNT.getSquadra();
+	    int image = getResources().getIdentifier(nomeSquadra.toLowerCase(),"drawable", getActivity().getPackageName());
+    	imageTeam.setImageResource(image);
 
 	    return rootView;
     }
 
+	public class MyListenerSpinner implements OnItemSelectedListener{
+		
+		@Override
+ 	    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+ 		 	Cursor cursor = (Cursor) arg0.getItemAtPosition(arg2);
+ 		 	giornataScelta = cursor.getString(0);
 
+
+ 		 	db.open();
+ 		 	String sql="SELECT "+ MyDatabase.PartiteMetaData.PARTITA_ID
+ 		 			+", "+MyDatabase.PartiteMetaData.PARTITA_SQ_CASA
+ 		 			+ ", "+ MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE 
+ 		 			+ " FROM "+ MyDatabase.PartiteMetaData.PARTITA_TITLE_TABLE
+ 		 			+ " WHERE "+MyDatabase.PartiteMetaData.PARTITA_IDGIORNATA
+ 		 			+ "='"+giornataScelta+"'";
+ 		 	Cursor c = db.query(sql, null);
+
+ 		 	SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+ 			    		getView().getContext(), 
+ 			    		R.layout.list_item_view_partite, 
+ 			    		c, 
+ 			    		new String[]{MyDatabase.PartiteMetaData.PARTITA_SQ_CASA, MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE}, 
+ 			    		new int[] {R.id.sqCasa, R.id.sqOspite},
+ 			    		1);
+
+
+ 		 	listaPartite.setAdapter(adapter);
+ 			db.close();
+
+ 	 	}
+
+ 	    @Override
+ 	    public void onNothingSelected(AdapterView<?> arg0) {
+ 	        // TODO Auto-generated method stub
+ 	    }   
+	}
+
+	
+	public class MyListnerListView implements OnItemClickListener{
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view,
+				int position, long id) {
+			Cursor c=(Cursor)parent.getItemAtPosition(position);
+			String sqCasa=c.getString(1);
+			String sqOspite=c.getString(2);
+
+			Bundle args = new Bundle();
+			args.putString(MyDatabase.PartiteMetaData.PARTITA_SQ_CASA, sqCasa);
+			args.putString(MyDatabase.PartiteMetaData.PARTITA_SQ_OSPITE, sqOspite);
+			args.putString(MyDatabase.PartiteMetaData.PARTITA_IDGIORNATA,giornataScelta);
+			Fragment fragment =new DettagliPartita();
+ 			FragmentManager fragmentManager = getFragmentManager();
+ 			fragment.setArguments(args);
+ 	        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+
+		}
+	}
 }
