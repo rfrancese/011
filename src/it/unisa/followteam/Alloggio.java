8fff186bfbf2817f010918506ec88c3b76f8ec81
Luigi Amitrano <l.amitrano@studenti.unisa.it>
2014-05-17 17:41:22 +0200

    aggiunta classe alloggio
    Signed-off-by: Luigi Amitrano <l.amitrano@studenti.unisa.it>

---
+++ src/it/unisa/followteam/Alloggio.java
@@ -0,0 +1,44 @@
+package it.unisa.followteam;
+
+
+public class Alloggio {
+
+	public Alloggio(double lat, double lon, String nome, String indir){
+		
+		latitudine=lat;
+		longitudine=lon;
+		nomeHotel=nome;
+		indirizzo=indir;
+		
+	}
+	
+	public double getLatitudine(){
+		
+		return latitudine;
+	}
+	
+	public double getLongitudine(){
+		
+		return longitudine;
+	}
+	
+	public String getNomeHotel(){
+	
+		return nomeHotel;
+	}
+	
+	public String getIndrizzo(){
+		
+		return indirizzo;
+	}
+	
+	public String toString(){
+		
+		return "latitudine: "+latitudine+" longitudine: "+longitudine+" nome hotel: "+nomeHotel+" indirizzo hotel: "+indirizzo;
+	}
+	
+	private double latitudine;
+	private double longitudine;
+	private String nomeHotel;
+	private String indirizzo;
+}
