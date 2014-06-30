84763b1643c2f81f21684f69ede6a05ff7b59257
Rita D'Ambrosio <r.dambrosio23@studenti.unisa.it>
2014-06-30 18:48:21 +0200

    gestione gps ultimato
    Signed-off-by: Rita D'Ambrosio <r.dambrosio23@studenti.unisa.it>

--- src/it/unisa/followteam/MappaPercorso.java
+++ src/it/unisa/followteam/MappaPercorso.java
@@ -26,9 +26,11 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MappaPercorso extends Fragment {
 
@@ -40,6 +42,7 @@ public class MappaPercorso extends Fragment {
 	private Marker puntatore;
 	private View rootView;
 	private String url = "";
+	private LatLng coordinateStadio;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -70,76 +73,86 @@ public class MappaPercorso extends Fragment {
 	public void onResume() {
 		super.onResume();
 
+		Bundle args = getArguments();
+		map = fragment.getMap();
 
-		if (map == null) {
+		final double latitudine = Double.parseDouble(args
+				.getString(MyDatabase.StadioMetaData.STADIO_LAT));
+		final double longitudine = Double.parseDouble(args
+				.getString(MyDatabase.StadioMetaData.STADIO_LONG));
+		coordinateStadio = new LatLng(latitudine, longitudine);
 
-			Bundle args = getArguments();
-			map = fragment.getMap();
+		gps = new GPSTracker(getActivity().getApplicationContext());
 
-			final double latitudine = Double.parseDouble(args
-					.getString(MyDatabase.StadioMetaData.STADIO_LAT));
-			final double longitudine = Double.parseDouble(args
-					.getString(MyDatabase.StadioMetaData.STADIO_LONG));
-			LatLng coordinateStadio = new LatLng(latitudine, longitudine);
+		// se non c'è connessione e il gps è disabilitato
+		if (!gps.canGetLocation()) {
+			showAlterOpzioniGPS();
+		} else if (gps.getLocation() != null) { // se il segnale del gps è
+												// stabile
 
-			gps = new GPSTracker(getActivity().getApplicationContext());
-			
-			//se non c'è connessione e il gps è disabilitato
-			if(!gps.canGetLocation()){
-				showAlterOpzioniGPS();
-			}else if(gps.getLocation() != null){ // se il segnale del gps non è stabile
-				
-				myLat = gps.getLatitude();
-				myLong = gps.getLongitude();
-				LatLng myPosizione = new LatLng(myLat, myLong);
-				map.addPolyline(new PolylineOptions()
-						.add(myPosizione, coordinateStadio).width(5)
-						.color(Color.RED));
-				map.setMyLocationEnabled(true);
-				final CameraPosition MYPOSIZIONE = new CameraPosition.Builder()
-						.target(myPosizione).zoom(5).bearing(0).tilt(25).build();
-	
-				map.animateCamera(CameraUpdateFactory
-						.newCameraPosition(MYPOSIZIONE));
-			}
+			myLat = gps.getLatitude();
+			myLong = gps.getLongitude();
+			LatLng myPosizione = new LatLng(myLat, myLong);
+			map.addPolyline(new PolylineOptions()
+					.add(myPosizione, coordinateStadio).width(5)
+					.color(Color.RED));
+			map.setMyLocationEnabled(true);
+			final CameraPosition MYPOSIZIONE = new CameraPosition.Builder()
+					.target(myPosizione).zoom(5).bearing(0).tilt(25).build();
 
-			int sqCasa = args.getInt(DettagliPartita.ICON_SQUADRA_CASA);
-			BitmapDescriptor icon = BitmapDescriptorFactory
-					.fromResource(sqCasa);
-
-			puntatore = map.addMarker(new MarkerOptions().position(
-					coordinateStadio).title(
-					"Clicca sul logo per tracciare il percorso"));
-			puntatore.setIcon(icon);
-			puntatore.showInfoWindow();
-
-			map.setOnMarkerClickListener(new OnMarkerClickListener() {
-
-				// traccia il percorso dalla propria posizione allo stadio
-				// tramite maps
-				@Override
-				public boolean onMarkerClick(Marker marker) {
-					url = "http://maps.google.com/maps?f=d&daddr=" + latitudine
-							+ "," + longitudine + "&mode=driving";
-					Intent intent = new Intent(
-							android.content.Intent.ACTION_VIEW, Uri.parse(url));
-					intent.setClassName("com.google.android.apps.maps",
-							"com.google.android.maps.MapsActivity");
-					startActivity(intent);
-					return true;
-				}
-			});
+			map.animateCamera(CameraUpdateFactory
+					.newCameraPosition(MYPOSIZIONE));
+		} else { // gps instabile
+			Toast.makeText(
+					rootView.getContext(),
+					"Il segnale gps non è stabile\nConsigliamo di modificare le impostazioni",
+					Toast.LENGTH_LONG).show();
+			zoomSuStadio();
 
 		}
+
+		int sqCasa = args.getInt(DettagliPartita.ICON_SQUADRA_CASA);
+		BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(sqCasa);
+
+		puntatore = map.addMarker(new MarkerOptions()
+				.position(coordinateStadio).title(
+						"Clicca sul logo per tracciare il percorso"));
+		puntatore.setIcon(icon);
+		puntatore.showInfoWindow();
+
+		map.setOnMarkerClickListener(new OnMarkerClickListener() {
+
+			// traccia il percorso dalla propria posizione allo stadio
+			// tramite maps
+			@Override
+			public boolean onMarkerClick(Marker marker) {
+				url = "http://maps.google.com/maps?f=d&daddr=" + latitudine
+						+ "," + longitudine + "&mode=driving";
+				Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+						Uri.parse(url));
+				intent.setClassName("com.google.android.apps.maps",
+						"com.google.android.maps.MapsActivity");
+				startActivity(intent);
+				return true;
+			}
+		});
+
 	}
-	
+
+	public void zoomSuStadio() {
+		final CameraPosition MYSTADIO = new CameraPosition.Builder()
+				.target(coordinateStadio).zoom(5).bearing(0).tilt(25).build();
+
+		map.animateCamera(CameraUpdateFactory.newCameraPosition(MYSTADIO));
+	}
+
 	public void showAlterOpzioniGPS() {
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(rootView.getContext());
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+				rootView.getContext());
 
 		alertDialog.setTitle("Opzioni GPS");
 
-		alertDialog
-				.setMessage("Il Gps è disabilitato. Vuoi attivarlo?");
+		alertDialog.setMessage("Il Gps è disabilitato. Vuoi attivarlo?");
 
 		alertDialog.setPositiveButton("Si",
 				new DialogInterface.OnClickListener() {
@@ -154,9 +167,15 @@ public class MappaPercorso extends Fragment {
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.cancel();
+						zoomSuStadio();
 					}
 				});
 
 		alertDialog.show();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
 	}
 }
